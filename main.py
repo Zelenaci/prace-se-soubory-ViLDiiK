fdd0f5d28e981d942e657e6877938dab4b234216
ViLDiiK <vilim.ondrej@gmail.com>
2021-11-21 20:10:20 +0100

    dodelat 3

--- main.py
+++ main.py
@@ -14,19 +14,23 @@ Zadeje volbu:
 1) Chcete pracovat se souborem?
 2) Chcete vygenerovat náhodný soubor?
 """)
+
 volba1=int(input("Zadejte číslo volby: "))
 
 if volba1==1:
-    soubor1=open(input("Zadejte cestu k pracovnímu souboru: "))
+    soubor1=open(input("Zadejte cestu k pracovnímu souboru: "),"r")
     obsah=soubor1.read()
     soubor2=open(input("Zadejte název výstupního souboru: "),"w")
+    
     print("""
     Chcete:
     1) Převést soubor na malá písmena
     2) Nahradit znak jiným znakem
     3) Statistiku jednotlivých znaků
     """)
+    
     podvolba1=int(input("Zadejte číslo volby: "))
+    
     if podvolba1==1:
         prepis=obsah.lower()
         vystup=soubor2.write(prepis)
@@ -34,14 +38,31 @@ if volba1==1:
         Soubor se přepsal do cílového souboru.
         Soubor je zapsán pouze malými písmeny.
         """)
+    
     elif podvolba1==2:
         zmenit=input("zadejte znak, který chcete změnit: ")
         nahrada=input("zadejte znak, kterým chcete nahrazovat první znak: ")
-        done=obsah.replace(zmenit,nahrada).replace(zmenit.upper(),nahrada)
+        done=obsah.replace(zmenit.lower(),nahrada.lower()).replace(zmenit.upper(),nahrada.upper()) #Aby nedošlo ke změně velikostí písmen, použil jsem .lower() a .upper()
         vystup=soubor2.write(done)
         print("Znak byl úspěšně nahrazen.")
-    elif podvolba1==3:
-        print("Dodělat!!!")
+    
+    elif podvolba1==3:    
+        pismena=dict()
+        while True:
+            znak = soubor1.read(1).upper()
+            if znak == '':
+                break
+            elif znak in pismena.keys():
+                pismena[znak] += 1
+            else:
+                pismena[znak] = 1
+
+        nej = max(pismena.values())
+        for znak in sorted(pismena.keys()):
+            if znak.isalpha():
+                print('{1:8d}| {0} | {2}'.format(znak, pismena[znak], 50 * pismena[znak] // nej * '*'))
+
+
     else:
         print("Neplatná volba, ukončuji program!!!")
         
@@ -50,8 +71,35 @@ if volba1==1:
 
 
 elif volba1==2:
+    
+    vystupni_soubor=open(input("Zadejte jméno výstupního souboru: "),"w")
+    samohlasky = "aeiyou"
+    souhlasky = "qwrtpsdfghjklzxcvbnm"
+
     pocet_slov=int(input("Zadejte počet slov pro vygenerovaný soubor: "))
-    print(pocet_slov)
+
+    def gen_slov(minznak=3, maxznak=10):
+        vysledek = ""
+        pocet = randint(minznak,maxznak)
+        zacatek = randint(0,1)
+        for i in range(pocet):
+            if i % 2 == zacatek:
+                vysledek=vysledek + choice(samohlasky)
+            else:
+                vysledek = vysledek + choice(souhlasky)
+        return vysledek    
+
+    def gen_vet(minslov=3, maxslov=12):
+        vysledek=""
+        for i in range(randint(minslov,maxslov)):
+            vysledek = vysledek + gen_slov() + " "
+        return vysledek.capitalize()[0:-1]+"."
+
+    vysledek=vystupni_soubor.write(gen_vet())
+
+    print("Náhodná zpráva byla úspěšně vygenerována.")
+
+
 
 else:
     print("Neplatná volba, ukončuji program!!!")
