a6767c48a2c1bb6d1a9f6cb3a484553126d5a7dd
ViLDiiK <vilim.ondrej@gmail.com>
2021-11-22 08:45:11 +0100

    3 do souboru

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import os
 print("""
 Zadeje volbu:
 1) Chcete pracovat se souborem?
-2) Chcete vygenerovat náhodný soubor?
-""")
+2) Chcete vygenerovat náhodný soubor?""")
 
-volba1=int(input("Zadejte číslo volby: "))
+volba1=int(input("""
+Zadejte číslo volby: """))
 
 if volba1==1:
 
@@ -23,30 +23,37 @@ if volba1==1:
     Chcete:
     1) Převést soubor na malá písmena
     2) Nahradit znak jiným znakem
-    3) Statistiku jednotlivých znaků
-    """)
+    3) Statistiku jednotlivých znaků""")
     
-    podvolba1=int(input("Zadejte číslo volby: "))
+    podvolba1=int(input("""
+    Zadejte číslo volby: """))
     
     if podvolba1==1:
-        soubor1=open(input("Zadejte cestu k pracovnímu souboru: "),"r")
+        soubor1=open(input("""
+        Zadejte cestu k pracovnímu souboru: """),"r")
         obsah=soubor1.read()
-        soubor2=open(input("Zadejte název výstupního souboru: "),"w")
+        soubor2=open(input("""
+        Zadejte název výstupního souboru: """),"w")
         prepis=obsah.lower()
         vystup=soubor2.write(prepis)
         print("""
         Soubor se přepsal do cílového souboru.
+
         Soubor je zapsán pouze malými písmeny.
         """)
     
     elif podvolba1==2:
 
-        soubor1=open(input("Zadejte cestu k pracovnímu souboru: "),"r")
+        soubor1=open(input("""
+        Zadejte cestu k pracovnímu souboru: """),"r")
         obsah=soubor1.read()
-        soubor2=open(input("Zadejte název výstupního souboru: "),"w")
+        soubor2=open(input("""
+        Zadejte název výstupního souboru: """),"w")
         
-        zmenit=input("zadejte znak, který chcete změnit: ")
-        nahrada=input("zadejte znak, kterým chcete nahrazovat první znak: ")
+        zmenit=input("""
+        zadejte znak, který chcete změnit: """)
+        nahrada=input("""
+        zadejte znak, kterým chcete nahrazovat první znak: """)
         
         if zmenit.isalpha() == True and nahrada.isalpha() == True:
             done=obsah.replace(zmenit.lower(),nahrada.lower()).replace(zmenit.upper(),nahrada.upper()) #Aby nedošlo ke změně velikostí písmen, použil jsem .lower() a .upper()
@@ -60,13 +67,17 @@ if volba1==1:
             done=obsah.replace(zmenit,nahrada)
             vystup=soubor2.write(done)
 
-        print("Znak byl úspěšně nahrazen.")
+        print("""
+        Znak byl úspěšně nahrazen.
+        """)
     
     elif podvolba1==3:    
         
-        soubor1 = input("Zadejte cestu k pracovnímu souboru: ")
+        soubor1 = input("""
+        Zadejte cestu k pracovnímu souboru: """)
         obsah = open(soubor1,"r")
-        soubor2 = input("Zadejte cestu k výstupnímu souboru: ")
+        soubor2 = input("""
+        Zadejte cestu k výstupnímu souboru: """)
 
         pismena=dict()
         
@@ -82,10 +93,13 @@ if volba1==1:
         nej = max(pismena.values())
         for znak in sorted(pismena.keys()):
             if znak.isalpha():
-                done=print('{1:8d}| {0} | {2}'.format(znak, pismena[znak], 50 * pismena[znak] // nej * '*'))
+                print("{1:8d}| {0} | {2}".format(znak, pismena[znak], 50 * pismena[znak] // nej * '*'))
+                
 
     else:
-        print("Neplatná volba, ukončuji program!!!")
+        print("""
+        Neplatná volba, ukončuji program!!!
+        """)
         
 
 
@@ -93,11 +107,13 @@ if volba1==1:
 
 elif volba1==2:
     
-    vystupni_soubor=open(input("Zadejte jméno výstupního souboru: "),"w")
+    vystupni_soubor=open(input("""
+    Zadejte jméno výstupního souboru: """),"w")
     samohlasky = "aeiyou"
     souhlasky = "qwrtpsdfghjklzxcvbnm"
 
-    pocet_slov=int(input("Zadejte počet slov pro vygenerovaný soubor: "))
+    pocet_slov=int(input("""
+    Zadejte počet slov pro vygenerovaný soubor: """))
 
     def gen_slov(minznak=3, maxznak=10):
         vysledek = ""
@@ -118,9 +134,13 @@ elif volba1==2:
 
     vysledek=vystupni_soubor.write(gen_vet())
 
-    print("Náhodná zpráva byla úspěšně vygenerována.")
+    print("""
+    Náhodná zpráva byla úspěšně vygenerována.
+    """)
 
 
 
 else:
-    print("Neplatná volba, ukončuji program!!!")
+    print("""
+    Neplatná volba, ukončuji program!!!
+    """)
